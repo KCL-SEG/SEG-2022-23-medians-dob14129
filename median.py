857558c9e9367e777a23d2363c133ea34a77fecb
Benjamin Pateman <benjamin.pateman@kcl.ac.uk>
2022-09-27 23:36:17 +0100

    calculate median

--- median.py
+++ median.py
@@ -10,3 +10,12 @@ while True:
     else:
         break
 print(numbers)
+
+numbers.sort()
+length = len(numbers)
+if length % 2 == 0:
+    first = numbers[int((length / 2) - 1)]
+    second = numbers[int(length / 2)]
+    print((first + second) / 2)
+else:
+    print(numbers[int((length - 1) / 2)])
